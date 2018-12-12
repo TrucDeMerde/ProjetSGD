ef58346374c6fadb1ffc2d919f76b8f2f12de5f0
tf872254 <tf872254@MI105-03>
2018-12-12 14:17:50 +0100

    test

---
+++ SGD/src/main/java/fr/ufrsciencestech/sgd/App.java
@@ -0,0 +1,13 @@
+package fr.ufrsciencestech.sgd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
